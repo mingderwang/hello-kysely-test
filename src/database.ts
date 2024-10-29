802d3da780044b56e6ee4ae1319d18b515a4531b
mingderwang <mingderwang@me.com>
2024-10-29 16:17:34 +0800

    with test v0.0.2

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,11 @@
 import { Kysely } from 'kysely'
 import { PlanetScaleDialect } from 'kysely-planetscale'
 
-const db = new Kysely<Database>({
+export const db = new Kysely<Database>({
   dialect: new PlanetScaleDialect({
     host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD
   })
 })
-
-console.log(db)
 
